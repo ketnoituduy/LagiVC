ea44ccbbfab33cc4f5c98b5ab7b2afa9dffb192b
Ngo Han Vu <ngohanvu@Ngos-MacBook-Air.local>
2025-02-15 11:02:53 +0700

    cap nhat server 10

--- controllers/authController.js
+++ controllers/authController.js
@@ -229,16 +229,20 @@ const authController = {
             const user = await User.findById(decoded.userId);
 
             if (!user) {
-                return res.status(400).json({ message: 'Người dùng không tồn tại' });
+                return res.status(400).json({ message: "Người dùng không tồn tại" });
             }
 
-            // Cập nhật mật khẩu mới cho người dùng
-            user.password = newPassword;
+            // Mã hóa mật khẩu mới
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(newPassword, salt);
+
+            // Cập nhật mật khẩu mới
+            user.password = hashedPassword;
             await user.save();
 
-            res.status(200).json({ message: 'Mật khẩu đã được thay đổi thành công' });
+            res.status(200).json({ message: "Mật khẩu đã được thay đổi thành công" });
         } catch (err) {
-            res.status(400).json({ message: 'Token không hợp lệ hoặc đã hết hạn' });
+            res.status(400).json({ message: "Token không hợp lệ hoặc đã hết hạn" });
         }
     },
     //Lay lai Password bị quên
@@ -267,7 +271,7 @@ const authController = {
             }
 
             // So sánh mật khẩu nhập vào với mật khẩu đã hash trong database
-            const isMatch = bcrypt.compareSync(password,user.password);
+            const isMatch = bcrypt.compareSync(password, user.password);
             if (!isMatch) {
                 return res.status(401).json({ message: "Invalid credentials" });
             }
