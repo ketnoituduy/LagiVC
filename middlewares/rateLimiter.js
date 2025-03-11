c567bc68a6cae08efe180416547f37c8d6231bd5
Ngo Han Vu <ngohanvu@Ngos-MacBook-Air.local>
2025-03-11 15:09:57 +0700

    cap nhat server 11/3/2025 1

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -1,13 +1,15 @@
-import rateLimit from "express-rate-limit";
+const rateLimit = require("express-rate-limit");
 
-export const registerLimiter = rateLimit({
+const registerLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 phút
-    max: 5, // Tối đa 5 request trong 15 phút
+    max: 5, // Tối đa 5 lần đăng ký trong 15 phút
     message: { error: "Bạn đã thực hiện quá nhiều lần đăng ký, vui lòng thử lại sau." },
 });
 
-export const loginLimiter = rateLimit({
+const loginLimiter = rateLimit({
     windowMs: 10 * 60 * 1000, // 10 phút
     max: 10, // Tối đa 10 lần đăng nhập trong 10 phút
     message: { error: "Bạn đã nhập sai quá nhiều lần, vui lòng thử lại sau." },
 });
+
+module.exports = { registerLimiter, loginLimiter };
